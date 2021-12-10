84e34c893947d579987d0a8c044332ddf7c8bfc2
Mamnuya Rinki <69429491+mamnuya@users.noreply.github.com>
2021-12-09 21:58:53 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "Dear Nashima, welcome to the couch.\n";
+  std::cout<<"Thank you.\n";
+  std::cout<<"Sincerely,\n";
+  std::cout<<"Nobody\n";
+
+}
